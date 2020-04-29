25745fa400ff0261e2fe4a3a6f080e77d903f31b
tallernait <tallernait@gmail.com>
2020-04-28 22:48:51 -0400

    Correction Apgrade Angular 9.0

--- src/app/service/constans.ts
+++ src/app/service/constans.ts
@@ -10,80 +10,13 @@ export const _DATOS_ = {
     image: {
         logo: 'assets/logo-sudotacion.png',
         whatsapp: 'assets/logo-whatsapp.png',
+        facebook: 'assets/facebook.png',
+        instagram: 'assets/instagram.png',
         trabajador: 'assets/trabajador-uniforme-dotacion.png',
         lienas: 'assets/lineas.png',
         medios1: 'assets/mediosDePago.jpg',
         medios2: 'assets/mediosDePago_2.jpg',
     },
-    items:
-        [
-            {
-                image: 'assets/uni-1.jpg',
-                titulo: 'Parrillero 4 piesas anti-fluido',
-                prendas: 'Gorro Pirata anti-fluido negro franja Roja; Delantal Anti fluido Rojo ; 3. Blusa cullo V,confecionanada en anti-fluido con ribetes rojos ; Pantalón resortado anti-fluido negro',
-                decription: 'Diseño y tecnología textil confeccionado en telas  de alta calidad con respaldo de garantía de las fábricas de mayor prestigio.',
-            },
-            {
-                image: 'assets/uni-2.jpg',
-                titulo: 'Conjunto quirúrgico anti-fluido',
-                prendas: 'Hombre azul oscuro',
-                decription: 'Diseño y tecnología textil confeccionado en telas  de alta calidad con respaldo de garantía de las fábricas de mayor prestigio.',
-            },
-            {
-                image: 'assets/uni-3.jpg',
-                titulo: 'Chef tipo pirata rojo gris',
-                prendas: 'Gorro Pirata Rojo; Delantal Anti fluido Rojo; Blusa de cheff confeccionada en lino negro con apliques rojos; Pantalón anti negro',
-                decription: 'Diseño y tecnología textil confeccionado en telas  de alta calidad con respaldo de garantía de las fábricas de mayor prestigio.',
-            },
-            {
-                image: 'assets/uni-4.jpg',
-                titulo: 'Uniforme chef blanco anti-fluido',
-                prendas: 'Blusa chef lino blanca ribete negro; Pantalón anti-fluido negro; Delantal frances corto anti cheffsitos ',
-                decription: 'Diseño y tecnología textil confeccionado en telas  de alta calidad con respaldo de garantía de las fábricas de mayor prestigio.',
-            },
-            {
-                image: 'assets/uni-5.jpg',
-                titulo: 'Conjunto quirúrgico anti-fluido mas bata',
-                prendas: 'Bata 3/4 gabardina hombre ;Conjunto quirúrgico anti-fluido hombre azul oscuro',
-                decription: 'Diseño y tecnología textil confeccionado en telas  de alta calidad con respaldo de garantía de las fábricas de mayor prestigio.',
-            },
-            {
-                image: 'assets/uni-6.jpg',
-                titulo: 'Conjunto enfermera anti-fluido',
-                prendas: 'Gorro ant medico ;Blusa estam anti-fluido ;Pantalón anti-fluido rojo',
-                decription: 'Diseño y tecnología textil confeccionado en telas  de alta calidad con respaldo de garantía de las fábricas de mayor prestigio.',
-            },
-            {
-                image: 'assets/uni-7.jpg',
-                titulo: 'Conjunto quirúrgico anti-fluido hombre azul oscuro',
-                prendas: 'Conjunto quirúrgico anti-fluido hombre azul oscuro',
-                decription: 'Diseño y tecnología textil confeccionado en telas  de alta calidad con respaldo de garantía de las fábricas de mayor prestigio.',
-            },
-            {
-                image: 'assets/uni-8.jpg',
-                titulo: 'Conjounto Jeans Worker',
-                prendas: 'Chaqueta jeans; Jeans 14 onzas azul',
-                decription: 'Diseño y tecnología textil confeccionado en telas  de alta calidad con respaldo de garantía de las fábricas de mayor prestigio.',
-            },
-            {
-                image: 'assets/uni-9.jpg',
-                titulo: 'Conjunto Servicios General',
-                prendas: 'Conjunto Servicios General. ;Blusa azul hortecia ;Pantalon azul oscuro',
-                decription: 'Diseño y tecnología textil confeccionado en telas  de alta calidad con respaldo de garantía de las fábricas de mayor prestigio.',
-            },
-            {
-                image: 'assets/uni-10.jpg',
-                titulo: 'Overol dril',
-                prendas: 'Overol enterizo dril raza azul',
-                decription: 'Diseño y tecnología textil confeccionado en telas  de alta calidad con respaldo de garantía de las fábricas de mayor prestigio.',
-            },
-            {
-                image: 'assets/uni-11.jpg',
-                titulo: 'Camisa oxford manga corta',
-                prendas: 'Camisa oxford manga corta',
-                decription: 'Diseño y tecnología textil confeccionado en telas  de alta calidad con respaldo de garantía de las fábricas de mayor prestigio.',
-            }
-        ],
     contenido: {
         horario: 'Lunes a Viernes  9:00 am - 6:30 pm. Sábados de  9:00 am - 5:00 pm',
         nosotros: 'Nos dedicamos a la fabricación de ropa para el trabajo. y comercialización de productos de seguridad para el trabajo, en áreas de la Industria de la Construcción, Metalmecánica, Comercio y Servicios como: Cocina, Mesa, Bar, Línea Hospitalaria y Cosmetología y ejecutivos.',
